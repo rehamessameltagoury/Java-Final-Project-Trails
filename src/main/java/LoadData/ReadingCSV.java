6bb9bc8ab3ab632654b1e83a7973d20e47de4d73
unknown <rehameltagory@gmail.com>
2021-07-02 12:56:16 +0200

    finished til Q10

---
+++ src/main/java/LoadData/ReadingCSV.java
@@ -0,0 +1,55 @@
+package LoadData;
+
+import org.apache.commons.csv.CSVFormat;
+import smile.data.DataFrame;
+import smile.data.Tuple;
+import smile.io.Read;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.ListIterator;
+import java.util.stream.Collectors;
+
+public class ReadingCSV {
+    private DataFrame WuzzufDataframe;
+    public DataFrame readCSV(String path) {
+        CSVFormat format = CSVFormat.DEFAULT.withFirstRecordAsHeader ();
+        DataFrame df = null;
+        try {
+            df = Read.csv (path, format);
+            //System.out.println(df.summary ());
+            df = df.select ("Title","Company","Location","Type","Level","YearsExp","Country","Skills");
+            //System.out.println(df.summary ());
+        } catch (IOException | URISyntaxException e) {
+            e.printStackTrace ();
+        }
+        WuzzufDataframe = df;
+        // System.out.println (df.summary ());
+        return df;
+    }
+    public DataFrame getWuzzufDataFrame() {
+        return WuzzufDataframe;
+    }
+    public List<WuzzufEmpolyee> getWuzzufEmpolyeeList() {
+        assert WuzzufDataframe != null;
+        List<WuzzufEmpolyee> WuzzufEmpolyees = new ArrayList<>();
+        ListIterator<Tuple> iterator = WuzzufDataframe.stream ().collect (Collectors.toList ()).listIterator ();
+        while (iterator.hasNext ()) {
+            Tuple t = iterator.next ();
+            WuzzufEmpolyee emp= new WuzzufEmpolyee ();
+            //Title,Company,Location,Type,Level,YearsExp,Country,Skills
+            emp.setCompany((String) t.get("Company"));
+            emp.setCountry((String) t.get("Country"));
+            emp.setLevel((String) t.get("Level"));
+            emp.setLocation((String) t.get("Location"));
+            emp.setSkills((String) t.get("Skills"));
+            emp.setTitle((String) t.get("Title"));
+            emp.setType((String) t.get("Type"));
+            emp.setYearsExp((String) t.get("YearsExp"));
+            WuzzufEmpolyees.add (emp);
+        }
+        return WuzzufEmpolyees;
+    }
+}
---
+++ src/main/java/LoadData/Wuzzufoperationdf.java
@@ -0,0 +1,114 @@
+package LoadData;
+
+import org.knowm.xchart.*;
+import org.knowm.xchart.style.Styler;
+import smile.data.DataFrame;
+import smile.plot.swing.BarPlot;
+
+import java.awt.*;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+
+import static java.util.stream.Collectors.*;
+
+public class Wuzzufoperationdf {
+    public static void main(String[] args)
+    {
+        String WuzzufPath= "D:\\ITI\\java\\WuzzufProject\\src\\main\\resources\\Wuzzuf_Jobs.csv"; //to change
+        ReadingCSV WuzzufData= new ReadingCSV();
+        DataFrame Wuzzufdf=WuzzufData.readCSV(WuzzufPath);
+        System.out.println("1. Read data set and convert it to dataframe or Spark RDD and display some from it.");
+        System.out.println(Wuzzufdf);
+        System.out.println("2. Display structure and summary of the data.");
+        System.out.println(Wuzzufdf.structure());
+        System.out.println(Wuzzufdf.summary());
+        System.out.println("3. Clean the data (null, duplications)");    // TODO
+        Wuzzufdf=Wuzzufdf.omitNullRows();
+        System.out.println(Wuzzufdf);
+        System.out.println("4. Count the jobs for each company and display that in order (What are the most demanding companies for jobs?)");
+        List<WuzzufEmpolyee> wemps=WuzzufData.getWuzzufEmpolyeeList();
+        Map<String, Long> jobEachcompany =wemps.stream().collect(groupingBy(WuzzufEmpolyee::getCompany,counting()));
+        jobEachcompany.entrySet().stream()
+                .sorted(Map.Entry.comparingByValue())
+                .forEach(System.out::println);
+
+        // Create Chart
+        PieChart pieCh = new PieChartBuilder().width(800).height(600).title(Wuzzufoperationdf.class.getSimpleName()).build();
+        // Customize Chart
+        Color[] sliceColors = new Color[]{new Color(180, 68, 50), new Color(130, 105, 120), new Color(80, 143, 160)};
+        pieCh.getStyler().setSeriesColors(sliceColors);
+       // Series
+
+        jobEachcompany.forEach((k, v)->pieCh.addSeries(k,v));
+
+
+// Show it
+        new SwingWrapper(pieCh).displayChart();
+        try {
+            BitmapEncoder.saveBitmap(pieCh, "./PieChart", BitmapEncoder.BitmapFormat.PNG);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("6. Find out What are the most popular job titles? ");
+        Map<String, Long> JobTitle =wemps.stream().collect(groupingBy(WuzzufEmpolyee::getTitle,counting()));
+        JobTitle.entrySet().stream()
+                .sorted(Map.Entry.comparingByValue())
+                .forEach(System.out::println);
+        List<String> keyList = new ArrayList<String>(JobTitle.keySet());
+        List<Long> valueList = new ArrayList<Long>(JobTitle.values());
+        //System.out.println(keyList);
+        CategoryChart chart = new CategoryChartBuilder().width(1024).height(768).title("Job Titles").xAxisTitle("Titles").yAxisTitle
+              ("Counting").build();
+        //Customize the chart
+        chart.getStyler().setLegendPosition(Styler.LegendPosition.InsideNW);
+        chart.getStyler().setHasAnnotations(true);
+        chart.getStyler().setStacked(true);
+        // 3.Series
+        chart.addSeries("Job Title",keyList.subList(1,50),valueList.subList(1,50));
+        /* 4.Show it */
+       new SwingWrapper(chart).displayChart();
+        try {
+            BitmapEncoder.saveBitmap(chart, "./JobTitleChart", BitmapEncoder.BitmapFormat.PNG);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+      System.out.println("8. Find out the most popular areas?");
+        Map<String, Long> Areas =wemps.stream().collect(groupingBy(WuzzufEmpolyee::getLocation,counting()));
+        Areas.entrySet().stream()
+                .sorted(Map.Entry.comparingByValue())
+                .forEach(System.out::println);
+
+        keyList = new ArrayList<String>(Areas.keySet());
+         valueList = new ArrayList<Long>(Areas.values());
+        //System.out.println(keyList);
+        CategoryChart chart2 = new CategoryChartBuilder().width(1024).height(768).title("Areas").xAxisTitle("Titles").yAxisTitle
+                ("Counting").build();
+        //Customize the chart
+        chart2.getStyler().setLegendPosition(Styler.LegendPosition.InsideNW);
+        chart2.getStyler().setHasAnnotations(true);
+        chart2.getStyler().setStacked(true);
+        // 3.Series
+        chart2.addSeries("Area",keyList.subList(1,50),valueList.subList(1,50));
+        try {
+            BitmapEncoder.saveBitmap(chart2, "./AreasChart", BitmapEncoder.BitmapFormat.PNG);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        new SwingWrapper(chart2).displayChart();
+        System.out.println("10. Print skills one by one and how many each repeated and order the output to find out the most important skills required?");
+        Map<String, Long> Skills =wemps.stream().collect(groupingBy(WuzzufEmpolyee::getSkills,counting()));
+        Skills.entrySet().stream()
+                .sorted(Map.Entry.comparingByValue())
+                .forEach(System.out::println);
+
+        System.out.println("11. Factorize the YearsExp feature and convert it to numbers in new col. (Bounce )");
+        System.out.println("12. Apply K-means for job title and companies (Bounce )");
+
+
+    }
+
+}
