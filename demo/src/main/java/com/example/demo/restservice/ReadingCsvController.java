da0c85502cd1313450a0798d92d7b3ce66b4c21e
Mahmoud Mohamed Taha <43166822+MahmoudMTaha@users.noreply.github.com>
2021-07-02 16:54:07 +0200

    added readcsv resource for reading the csv file and send data back for client

---
+++ demo/src/main/java/com/example/demo/restservice/ReadingCsvController.java
@@ -0,0 +1,20 @@
+package com.example.demo.restservice;
+
+import com.example.demo.LoadData.ReadingCSV;
+import com.example.demo.LoadData.WuzzufEmpolyee;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import smile.data.DataFrame;
+
+import java.util.List;
+
+@RestController
+public class ReadingCsvController {
+    @GetMapping("/readcsv")
+    public List<WuzzufEmpolyee> read() {
+        ReadingCSV EmplyessFile = new ReadingCSV();
+        DataFrame empolyDF = EmplyessFile.readCSV("src/main/resources/Wuzzuf_Jobs.csv");
+        List<WuzzufEmpolyee> empolyeesList = EmplyessFile.getEmpolyeeList();
+        return empolyeesList;
+    }
+}
